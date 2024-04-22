ed5131bba8d1f69fbf8daf3e169a28fbfd87ff5d
rana <rm1706@fayoum.edu.eg>
2024-04-22 18:54:33 +0200

    Update

--- Repository/Authentication.cs
+++ Repository/Authentication.cs
@@ -1,8 +1,23 @@
-﻿
+﻿using DermDiag.DTO;
+using DermDiag.Models;
+
 namespace DermDiag.Repository
 {
     public class Authentication
     {
-        
+        private readonly DermDiagContext context1;
+        public Authentication(DermDiagContext context)
+        {
+            context1 = context;
+        }
+
+        public bool Login(LoginDTO login)
+        {
+            var c1 =
+            context1.Patients.FirstOrDefault((p) => p.Email == login.Email && p.Password == login.Password);
+            if (c1 != null) { return true; } else { return false; }
+
+
+        }
     }
 }
